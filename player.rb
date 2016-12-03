9afa62bb6ce89882ebb9deeee4336bd57aaa8e0e
Craig Morton <CraigMorton22@gmail.com>
2016-12-03 18:57:58 +0000

    Add name to player

--- player.rb
+++ player.rb
@@ -1,3 +1,7 @@
 class Player
 
+	def name
+		return "Craig"
+	end
+
 end
--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -4,8 +4,12 @@ require_relative("../player.rb")
 
 class PlayerSpec < MiniTest::Test
 
-	def test_can_create_player
-		player = Player.new()
+	def setup
+		@player = Player.new()
+	end
+
+	def test_player_has_name
+		assert_equal("Craig", @player.name)
 	end
 
 
