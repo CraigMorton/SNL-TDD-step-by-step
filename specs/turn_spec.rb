163051be3735e5443b759c96ca4f6537525f86aa
Craig Morton <CraigMorton22@gmail.com>
2016-12-07 17:41:05 +0000

    Add add player method to Turn

--- specs/turn_spec.rb
+++ specs/turn_spec.rb
@@ -1,6 +1,7 @@
 require("minitest/autorun")
 require("minitest/rg")
 require_relative("../turn.rb")
+require_relative("../player.rb")
 
 class TurnSpec < MiniTest::Test
 
@@ -10,6 +11,12 @@ class TurnSpec < MiniTest::Test
 
   def test_has_play_order_array
     assert_equal([], @turn.order)
+  end
+
+  def test_can_add_players_to_order
+    player = Player.new("Craig")
+    @turn.add(player)
+    assert_equal(player, @turn.order[0])
   end
 
 end
--- turn.rb
+++ turn.rb
@@ -1,7 +1,15 @@
 class Turn
 
+  def initialize
+    @order = []
+  end
+
   def order
-    []
+    @order
+  end
+
+  def add(player)
+    @order.push(player)
   end
 
 end
