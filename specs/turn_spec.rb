c3d000aa7856ae21a37c7d0b100defe0f200ac45
Craig Morton <CraigMorton22@gmail.com>
2016-12-08 12:18:03 +0000

    Add multiple players test

--- specs/turn_spec.rb
+++ specs/turn_spec.rb
@@ -8,6 +8,8 @@ class TurnSpec < MiniTest::Test
   def setup
     @turn = Turn.new
     @player = Player.new("Craig")
+    @player2 = Player.new("John")
+    @player3 = Player.new("Jarrod")
   end
 
   def test_has_play_order_array
@@ -23,6 +25,14 @@ class TurnSpec < MiniTest::Test
     @turn.add(@player)
     @turn.order[0] = "!!!"
     assert_equal(@player, @turn.order[0])
+  end
+
+  def test_can_add_multiple_players
+    @turn.add(@player)
+    @turn.add(@player2)
+    @turn.add(@player3)
+    assert_equal(3, @turn.order.size)
+    assert_equal(@player3, @turn.order[2])
   end
 
 end
