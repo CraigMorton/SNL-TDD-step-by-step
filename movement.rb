6357a6e5b34b4435d0fbf8fbd88a7cbe9403ed67
Craig Morton <CraigMorton22@gmail.com>
2016-12-13 01:28:26 +0000

    Add management of player roll and position update to Movement class

--- movement.rb
+++ movement.rb
@@ -1,7 +1,14 @@
 class Movement
 
 	def initialize(board, player_list, turn_order)
-		
+		@board = board
+		@player_list = player_list
+		@turn_order = turn_order
+	end
+
+	def roll(dice)
+		player = @turn_order.current
+		@player_list.update_position(player, player.roll(dice))
 	end
 
 end
--- specs/movement_spec.rb
+++ specs/movement_spec.rb
@@ -4,14 +4,26 @@ require_relative("../movement.rb")
 require_relative("../board.rb")
 require_relative("../player_list.rb")
 require_relative("../turn.rb")
+require_relative("../player.rb")
+require_relative("../dice.rb")
 
 class MovementSpec < MiniTest::Test
 
-	def test_takes_board_and_player_list_in_initialize
-		board = Board.new(0, {})
-		list = PlayerList.new
-		turn = Turn.new
-		movement = Movement.new(board, list, turn)
+	def setup
+		@board = Board.new(5, {})
+		@list = PlayerList.new
+		@turn = Turn.new
+		@movement = Movement.new(@board, @list, @turn)
+	end
+
+	def test_manages_player_roll
+		player = Player.new("Craig")
+		@list.add(player)
+		@turn.add(player)
+
+		roll_one = Dice.new(1)
+		@movement.roll(roll_one)
+		assert_equal(1, @list.position(player))
 	end
 
 end
